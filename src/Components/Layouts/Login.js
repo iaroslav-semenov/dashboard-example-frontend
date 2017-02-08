d4c92db47517f9fbf217f0e63b1495554ef02ce2
Iaroslav Semenov <iaroslav.semenov@movago.de>
2017-02-08 17:31:40 +0100

    Login layout fixed

--- src/Components/Layouts/Login.js
+++ src/Components/Layouts/Login.js
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import { render } from 'react-dom';
 import { Container, Row, Col } from 'reactstrap';
-import { Card, CardBlock, CardTitle } from 'reactstrap';
+import { Card, CardBlock, CardHeader } from 'reactstrap';
 import { Form, FormGroup, Input, Button } from 'reactstrap';
 
 import style from '../../Styles/login.scss';
@@ -9,13 +9,12 @@ import style from '../../Styles/login.scss';
 class Login extends React.Component {
     render() {
         return (
-            <Container className="align-slef-center">
-                <Row className="justify-content-center">
+            <Container className="h100p">
+                <Row className="justify-content-center h100p">
                     <Col sm="6" className="align-self-center">
                         <Card>
+                            <CardHeader>Login</CardHeader>
                             <CardBlock>
-                                <CardTitle>Login</CardTitle>
-                                <p className="hint-text">some text in red</p>
                                 <Form>
                                     <FormGroup>
                                         <Input name="login" placeholder="Login" type="text"/>
@@ -23,7 +22,14 @@ class Login extends React.Component {
                                     <FormGroup>
                                         <Input name="password" placeholder="Password" type="password"/>
                                     </FormGroup>
-                                    <Button type="submit">Let me in</Button>
+                                    <FormGroup>
+                                        <Button
+                                            type="submit"
+                                            outline color="primary"
+                                            className="float-sm-right">
+                                            Let me in
+                                        </Button>
+                                    </FormGroup>
                                 </Form>
                             </CardBlock>
                         </Card>
--- src/app.js
+++ src/app.js
@@ -6,5 +6,6 @@ import Root from './Components/Root';
 
 /** Styles */
 import 'bootstrap/dist/css/bootstrap.css';
+import style from './Styles/global.scss';
 
 render(<Root history={ browserHistory } />, document.getElementById('app'));
